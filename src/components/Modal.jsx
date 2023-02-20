f0db35f5d66f72e64b94fb25101aa75fda7d3afd
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2023-02-20 13:51:38 +0900

    fix: カードの見た目を大きなデスクトップにも対応させる

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,9 +11,7 @@ const Modal = (props) => {
         <button onClick={() => setShowModal(true)}>
           <WorksCard
             title={props.title}
-            comment={
-              props.subtitle
-            }
+            comment={props.subtitle}
             imgPath={props.thumbnail}
           />
         </button>
@@ -31,35 +29,39 @@ const Modal = (props) => {
                       {props.title}
                     </h3>
                     <div className="flex items-center">
-                    <div className="text-blue-300 mx-auto px-4">
-                      {props.links}
-                    </div> 
-                    <button
-                      className="text-blue-300 background-transparent font-bold uppercase px-2 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-200 hover:text-blue-500 hover:-translate-y-1 md:ml-auto"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="icon icon-tabler icon-tabler-x"
-                        width="32"
-                        height="32"
-                        viewBox="0 0 24 24"
-                        stroke-width="2"
-                        stroke="currentColor"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                      <div className="text-blue-300 mx-auto px-4">
+                        {props.links}
+                      </div>
+                      <button
+                        className="text-blue-300 background-transparent font-bold uppercase px-2 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-200 hover:text-blue-500 hover:-translate-y-1 md:ml-auto"
+                        type="button"
+                        onClick={() => setShowModal(false)}
                       >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M18 6l-12 12"></path>
-                        <path d="M6 6l12 12"></path>
-                      </svg>
-                    </button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="icon icon-tabler icon-tabler-x"
+                          width="32"
+                          height="32"
+                          viewBox="0 0 24 24"
+                          stroke-width="2"
+                          stroke="currentColor"
+                          fill="none"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path
+                            stroke="none"
+                            d="M0 0h24v24H0z"
+                            fill="none"
+                          ></path>
+                          <path d="M18 6l-12 12"></path>
+                          <path d="M6 6l12 12"></path>
+                        </svg>
+                      </button>
                     </div>
                   </div>
                   <div className="text-lg text-gray-500 pl-6 mb-2">
-                      {props.date}
+                    {props.date}
                   </div>
                 </div>
                 {/*body*/}
@@ -71,14 +73,14 @@ const Modal = (props) => {
                           Description
                         </h3>
                         <div className="">
-                        <p className=" text-gray-500 leading-relaxed text-left">
-                          {props.description}
-                        </p>
+                          <p className="text-gray-500 leading-relaxed text-left">
+                            {props.description}
+                          </p>
                         </div>
                       </div>
                       <div className="mb-2">
                         <h3 className="text-lg text-blue-400 font-semibold">
-                            Skills
+                          Skills
                         </h3>
                         <p className="ml-6 text-gray-500 leading-relaxed">
                           {props.skills}
@@ -86,19 +88,89 @@ const Modal = (props) => {
                       </div>
                     </div>
                     <div className="md:w-2/5 w-3/4 mb-4">
-                      <div className="flex mx-auto mb-2 justify-center">
-                        <button onClick={() => setSelectImg(0)}>
-                          <img className="rounded-sm p-1 hover:-translate-y-1 hover:shadow-md duration-100" src={props.imgPaths[0]} alt="" />
+                      <div className="flex mx-auto mb-2 justify-center px-10">
+                        <button className="p-2 hover:text-blue-400 text-gray-500 duration-150" onClick={() => setSelectImg(0)}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-circle-filled"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path
+                              stroke="none"
+                              d="M0 0h24v24H0z"
+                              fill="none"
+                            ></path>
+                            <path
+                              d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z"
+                              stroke-width="0"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
                         </button>
-                        <button onClick={() => setSelectImg(1)}>
-                          <img className="rounded-sm p-1 hover:-translate-y-1 hover:shadow-md duration-100" src={props.imgPaths[1]} alt="" />
+                        <button className="p-2 hover:text-blue-400 text-gray-500 duration-150" onClick={() => setSelectImg(1)}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-circle-filled"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path
+                              stroke="none"
+                              d="M0 0h24v24H0z"
+                              fill="none"
+                            ></path>
+                            <path
+                              d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z"
+                              stroke-width="0"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
                         </button>
-                        <button onClick={() => setSelectImg(2)}>
-                          <img className="rounded-sm p-1 hover:-translate-y-1 hover:shadow-md duration-100" src={props.imgPaths[2]} alt="" />
+                        <button className="p-2 hover:text-blue-400 text-gray-500 duration-150" onClick={() => setSelectImg(2)}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-circle-filled"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path
+                              stroke="none"
+                              d="M0 0h24v24H0z"
+                              fill="none"
+                            ></path>
+                            <path
+                              d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z"
+                              stroke-width="0"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
                         </button>
                       </div>
                       <div className="flex justify-center">
-                          <img className="rounded-lg" src={props.imgPaths[selectImg]} alt="" /> 
+                        <img
+                          className="rounded-lg"
+                          src={props.imgPaths[selectImg]}
+                          alt=""
+                        />
                       </div>
                     </div>
                   </div>
